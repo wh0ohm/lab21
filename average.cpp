c50d3a4c7483014c5ed195bc192566f3b9539d34
wh0ohm <wararat.boonchieng@gmail.com>
2025-02-21 14:36:31 +0700

    complete

--- average.cpp
+++ average.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
+#include<cstdlib>
+int main(int argc , char* argv[]){
+    if(argc == 1){
+        cout << "Please input numbers to find average.";
+    }
+    else {
+        double mean,sum=0;
+        for(int i=1;i<argc;i++){
+            sum+=atof(argv[i]);
+            mean=sum/(argc-1);
+        }
+    cout<<"---------------------------------\n";
+    cout << "Average of " << argc-1 << " numbers = " << mean << '\n';
+    cout<<"---------------------------------\n";
+    }
+    return 0;
+}
